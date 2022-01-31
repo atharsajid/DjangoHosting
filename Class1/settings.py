851d367c332e108e8093d0c39205c41caadc4ece
atharsajid <athersajid620@gmail.com>
2022-01-31 10:42:29 -0800

    update

--- Class1/settings.py
+++ Class1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m!0ep)1)8$6xjd&8*fn+k^78w-98gkvuy5=%(p-4dixv&m1h7v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['jawanpakisatantodo.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = []
 
 
 # Application definition
